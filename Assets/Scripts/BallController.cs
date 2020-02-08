e5fb82e36067e010d6d7125c2de185dbd1d015ad
DuRaRaRall <alstonezhang@foxmail.com>
2020-02-07 23:03:26 -0800

    First Try

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody player_rg;
+    private Rigidbody ball_rg;
+    private bool isPickUp;
+
+    private Vector3 carry_offset;
+
+    public Button button;
+    public float shot_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPickUp = false;
+        carry_offset = new Vector3(1.5f, 3.0f, 1.5f);
+        button.onClick.AddListener(TaskOnClick);
+        ball_rg = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPickUp)
+        {
+            transform.position = player_rg.position + carry_offset;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.CompareTo("Player") == 0 && !isPickUp)
+        {
+            player_rg = collision.collider.attachedRigidbody;
+            isPickUp = true;
+        }
+    }
+
+    void TaskOnClick()
+    {
+        if (isPickUp)
+        {
+            
+            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+            ball_rg.velocity = player_rg.transform.forward * shot_speed;
+            isPickUp = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float timer;
+    private bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
+        {
+            if(timer > 1)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                timer += 0.03f;
+            }
+            
+        }
+        
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if ((other.tag.CompareTo("Player") == 0 || other.tag.CompareTo("Bound") == 0)&& flag == false)
+        {
+            flag = true;
+            timer = 0;
+        }
+    }
+}
